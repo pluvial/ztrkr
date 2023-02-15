7fb0e9ae3a1d037a76a5a07db1f8871e867da344
jpaquim <joaopaquim@gmail.com>
2023-02-15 17:02:54 +0000

    Use svelte variables instead of stores

--- src/lib/Ztrkr.svelte
+++ src/lib/Ztrkr.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { onMount, tick } from 'svelte';
-	import type { Writable } from 'svelte/store';
+	import { onMount } from 'svelte';
 	import Controls from './Controls.svelte';
 	// import Display from './Display.svelte';
 	import Keyboard from './Keyboard.svelte';
@@ -12,76 +11,78 @@
 	import { defaultPattern, defaultProject, defaultTracks } from './state';
 	import { type N16, array16V, bound, t16, type Tuple16 } from './utils';
 
-	export let projectIndex: Writable<number>;
-	export let patternIndex: Writable<number>;
-	// export let patternIndex: Writable<N16>;
-	export let trackIndex: Writable<N16>;
+	export let disk: Disk;
 
-	export let disk: Writable<Disk>;
-	export let projects: Writable<Project[]>;
-	export let patterns: Writable<Pattern[]>;
-	export let tracks: Writable<Tuple16<Track>>;
+	let projects: Project[];
+	let patterns: Pattern[];
+	let tracks: Tuple16<Track>;
 
-	$: project = $projects[$projectIndex];
-	$: pattern = $patterns[$patternIndex];
-	$: track = $tracks[$trackIndex];
+	let projectIndex = 0;
+	let patternIndex = 0;
+	//  let patternIndex: N16 = 0
+	let trackIndex: N16 = 0;
 
 	$: {
-		$projects = $disk.projects;
-		$projectIndex = 0;
+		projects = disk.projects;
+		projectIndex = 0;
 	}
+
+	$: project = projects[projectIndex];
+	$: pattern = patterns[patternIndex];
+
+	$: tracks = pattern.tracks;
+	$: track = tracks[trackIndex];
 	$: {
-		$patterns = project.patterns;
-		$patternIndex = 0;
+		patterns = project.patterns;
+		patternIndex = 0;
 	}
-	$: $tracks = pattern.tracks;
 
 	// debug logging
-	$: console.debug({ $disk });
-	$: console.debug({ $projects });
-	$: console.debug({ $patterns });
-	$: console.debug({ $tracks });
+	$: console.debug({ disk });
+	$: console.debug({ projects });
+	$: console.debug({ patterns });
+	$: console.debug({ tracks });
 
 	$: console.debug({ pattern });
 	$: console.debug({ project });
 	$: console.debug({ track });
 
 	function newProject() {
-		$projects = [...$projects, defaultProject()];
-		$projectIndex = $projects.length - 1;
+		projects = [...projects, defaultProject()];
+		projectIndex = projects.length - 1;
 	}
 
 	function newPattern() {
-		$patterns = [...$patterns, defaultPattern()];
-		$patternIndex = $patterns.length - 1;
+		patterns = [...patterns, defaultPattern()];
+		patternIndex = patterns.length - 1;
 	}
 
 	function clearPattern() {
-		$tracks = defaultTracks();
+		tracks = defaultTracks();
 	}
 
 	function clearTracks() {
 		for (const t of t16) {
-			$tracks[t].steps = Array.from({ length: lengths[t] });
+			tracks[t].steps = Array.from({ length: lengths[t] });
 		}
 	}
 
 	function clearTrack(t: N16) {
-		$tracks[t].steps = Array.from({ length: lengths[t] });
+		tracks[t].steps = Array.from({ length: lengths[t] });
 	}
 
 	function setTrack(index: N16) {
-		$trackIndex = index;
+		trackIndex = index;
 	}
 
 	function selectNextTrack() {
-		const len = $tracks.length;
-		setTrack((($trackIndex + 1) % len) as N16);
+		const len = tracks.length;
+		setTrack(((trackIndex + 1) % len) as N16);
 	}
 
 	function selectPrevTrack() {
-		const len = $tracks.length;
-		setTrack((($trackIndex + len - 1) % len) as N16);
+		const len = tracks.length;
+		setTrack(((trackIndex + len - 1) % len) as N16);
 	}
 
 	$: activeTracks = t16.filter(t => !project.mutes.has(t) && !pattern.mutes.has(t));
@@ -94,11 +95,11 @@
 	function setBPM(value: number) {
 		value = bound(value, 20, 400);
 		if (tempoMode === 'global') {
-			$projects[$projectIndex].tempo = value;
+			projects[projectIndex].tempo = value;
 			// project.tempo = value;
 		} else {
-			$patterns[$patternIndex].tempo = value;
-			// $pattern.tempo = value;
+			patterns[patternIndex].tempo = value;
+			// pattern.tempo = value;
 		}
 	}
 
@@ -107,34 +108,34 @@
 	$: scales =
 		scaleMode === 'per-pattern'
 			? array16V(pattern.scale as number)
-			: $tracks.map(track => track.scale as number);
+			: tracks.map(track => track.scale as number);
 
-	$: scale = scales[$trackIndex];
+	$: scale = scales[trackIndex];
 
 	function setScale(value: number) {
 		value = bound(value, 1 / 16, 4);
 		if (scaleMode === 'per-pattern') {
-			$patterns[$patternIndex].scale = value;
-			// $pattern.scale = value;
+			patterns[patternIndex].scale = value;
+			// pattern.scale = value;
 		} else {
-			$tracks[$trackIndex].scale = value;
-			// $track.scale = value;
+			tracks[trackIndex].scale = value;
+			// track.scale = value;
 		}
 	}
 
 	$: lengths =
-		scaleMode === 'per-pattern' ? array16V(pattern.length) : $tracks.map(t => t.length as number);
+		scaleMode === 'per-pattern' ? array16V(pattern.length) : tracks.map(t => t.length as number);
 
-	$: length = lengths[$trackIndex];
+	$: length = lengths[trackIndex];
 
 	function setLength(value: number) {
 		value = bound(value, 1, 64);
 		if (scaleMode === 'per-pattern') {
-			$patterns[$patternIndex].length = value;
-			// $pattern.length = value;
+			patterns[patternIndex].length = value;
+			// pattern.length = value;
 		} else {
-			$tracks[$trackIndex].length = value;
-			// $track.length = value;
+			tracks[trackIndex].length = value;
+			// track.length = value;
 		}
 	}
 
@@ -145,11 +146,11 @@
 		[] as number[],
 	);
 
-	function toggleStep(step: number, t = $trackIndex) {
-		if ($tracks[t].steps[step]) {
-			$tracks[t].steps[step] = undefined;
+	function toggleStep(step: number, t = trackIndex) {
+		if (tracks[t].steps[step]) {
+			tracks[t].steps[step] = undefined;
 		} else {
-			$tracks[t].steps[step] = { type: 'note' };
+			tracks[t].steps[step] = { type: 'note' };
 		}
 	}
 
@@ -197,7 +198,7 @@
 </script>
 
 <Player
-	tracks={$tracks}
+	{tracks}
 	activeTracks={activeTracksSet}
 	{bpm}
 	{lengths}
@@ -217,7 +218,7 @@
 			on:stop={stop}
 			{scale}
 			on:pattern-clear={clearPattern}
-			on:track-clear={() => clearTrack($trackIndex)}
+			on:track-clear={() => clearTrack(trackIndex)}
 			on:tracks-clear={clearTracks}
 			on:track-change={({ detail: t }) => setTrack(t)}
 			on:track-prev={selectPrevTrack}
@@ -232,30 +233,30 @@
 		>
 			<!-- <Display /> -->
 			<Keys
-				highlighted={[patternSteps[$trackIndex]]}
+				highlighted={[patternSteps[trackIndex]]}
 				active={activeSteps}
 				pressed={pressedSteps}
 				{showKeys}
 				on:click={({ detail: step }) => toggleStep(step)}
 			/>
 			<Tracker
-				selectedTrack={$trackIndex}
+				selectedTrack={trackIndex}
 				{lengths}
 				{scales}
 				{patternSteps}
 				{showKeys}
-				tracks={$tracks}
+				{tracks}
 				on:track-change={({ detail: t }) => setTrack(t)}
 				on:step-toggle={({ detail: step }) => toggleStep(step)}
 			/>
 		</Keyboard>
 
 		<Controls
-			projectIndex={$projectIndex}
+			{projectIndex}
 			projectName={project.name ?? 'Undefined'}
-			patternIndex={$patternIndex}
+			{patternIndex}
 			patternName={pattern.name ?? 'Undefined'}
-			trackIndex={$trackIndex}
+			{trackIndex}
 			{playing}
 			on:play={play}
 			on:pause={pause}
@@ -265,32 +266,32 @@
 			}}
 			{tempoMode}
 			on:tempo-mode-change={({ detail: tempoMode }) =>
-				($patterns[$patternIndex].tempoMode = tempoMode)}
+				(patterns[patternIndex].tempoMode = tempoMode)}
 			{bpm}
 			on:bpm-change={({ detail: bpm }) => setBPM(bpm)}
 			{scaleMode}
 			on:scale-mode-change={({ detail: scaleMode }) =>
-				($patterns[$patternIndex].scaleMode = scaleMode)}
+				(patterns[patternIndex].scaleMode = scaleMode)}
 			{scale}
 			on:scale-change={({ detail: scale }) => setScale(scale)}
 			{length}
 			on:length-change={({ detail: length }) => setLength(length)}
 			noteNumber={track.noteNumber}
 			on:note-number-change={({ detail: noteNumber }) =>
-				($tracks[$trackIndex].noteNumber = noteNumber)}
+				(tracks[trackIndex].noteNumber = noteNumber)}
 			velocity={track.velocity}
 			on:velocity-change={({ detail: velocity }) =>
-				($tracks[$trackIndex].velocity = bound(Math.round(velocity), 0, 127))}
+				(tracks[trackIndex].velocity = bound(Math.round(velocity), 0, 127))}
 			probability={track.probability}
 			on:probability-change={({ detail: probability }) =>
-				($tracks[$trackIndex].probability = bound(probability, 0, 1))}
+				(tracks[trackIndex].probability = bound(probability, 0, 1))}
 			on:project-prev={() =>
-				($projectIndex = ($projectIndex + $projects.length - 1) % $projects.length)}
-			on:project-next={() => ($projectIndex = ($projectIndex + 1) % $projects.length)}
+				(projectIndex = (projectIndex + projects.length - 1) % projects.length)}
+			on:project-next={() => (projectIndex = (projectIndex + 1) % projects.length)}
 			on:project-new={newProject}
 			on:pattern-prev={() =>
-				($patternIndex = ($patternIndex + $patterns.length - 1) % $patterns.length)}
-			on:pattern-next={() => ($patternIndex = ($patternIndex + 1) % $patterns.length)}
+				(patternIndex = (patternIndex + patterns.length - 1) % patterns.length)}
+			on:pattern-next={() => (patternIndex = (patternIndex + 1) % patterns.length)}
 			on:pattern-new={newPattern}
 			on:track-prev={selectPrevTrack}
 			on:track-next={selectNextTrack}
