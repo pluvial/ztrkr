2fcec87262fd074236cc590f859d1faf3e1ab8ab
jpaquim <joaopaquim@gmail.com>
2023-02-28 23:06:51 +0000

    Move color palette to css file
    Disable debug palette

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import './palette.css';
 	import './styles.css';
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Palette from '$lib/Palette.svelte';
+	// import Palette from '$lib/Palette.svelte';
 	import Ztrkr from '$lib/Ztrkr.svelte';
 	import * as state from '$lib/state';
 	import * as storage from '$lib/storage';
@@ -11,4 +11,4 @@
 
 <Ztrkr bind:disk on:storage-clear={storage.clear} />
 
-<Palette />
+<!-- <Palette /> -->
