6a8801e4a08910c49f4bb6b8071c01f51f0c4e85
jpaquim <joaopaquim@gmail.com>
2023-03-02 13:50:39 +0000

    Add route with multiple trackers

---
+++ src/routes/multiple/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Ztrkr from '$lib/Ztrkr.svelte';
+	import * as state from '$lib/state';
+	import * as storage from '$lib/storage';
+
+	let disk = storage.get() ?? state.defaultDisk();
+
+	$: storage.set(disk);
+</script>
+
+<div>
+	<Ztrkr bind:disk on:storage-clear={storage.clear} />
+	<Ztrkr bind:disk on:storage-clear={storage.clear} />
+	<Ztrkr bind:disk on:storage-clear={storage.clear} />
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 2em;
+	}
+</style>
